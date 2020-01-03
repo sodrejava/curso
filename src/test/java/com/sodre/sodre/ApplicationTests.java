29715198bdd503296cbdf7c61aecd6efe7677a36
Sodre <sodrejava@gmail.com>
2020-01-03 18:10:39 -0300

    criado

---
+++ src/test/java/com/sodre/sodre/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sodre.sodre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
